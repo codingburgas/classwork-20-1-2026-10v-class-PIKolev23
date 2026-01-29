25c7a3987099c77eb72b860578aa35217c9e56bf
PIKolev23 <PIKolev23@codingburgas.bg>
2026-01-29 14:53:41 +0200

    Add files via upload
    2. Разлики между статични и динамични библиотеки

---
+++ MathLibrary/StudentWork/StudentWork.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "..\MathLibrary\MathLibrary.h"
+
+using namespace std;
+
+int main()
+{
+    cout << add(5, 3) << endl;
+    cout << sub(10, 4) << endl;
+    return 0;
+}
